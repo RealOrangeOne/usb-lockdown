fab45c353afd2a46ea7128a1646aa7882c2f9a66
Jake Howard <jake3497@hotmail.co.uk>
2014-08-06 22:30:20 +0100

    init all files

---
+++ USB Lockdown/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace USB_Lockdown
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            if (args.Length == 0)
+            {
+                Application.Run()
+            }
+            else if (args[0] == "LOCK")
+            {
+                Application.Run(new Form1());
+            }
+        }
+    }
+}

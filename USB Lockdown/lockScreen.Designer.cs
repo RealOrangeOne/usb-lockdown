34da26901a7328da8726d089101f9565a6a5ddbf
theorangeone97 <jake3497@hotmail.co.uk>
2014-08-18 12:49:25 +0100

    added testing controls

--- USB Lockdown/lockScreen.Designer.cs
+++ USB Lockdown/lockScreen.Designer.cs
@@ -30,21 +30,69 @@
         {
             this.components = new System.ComponentModel.Container();
             this.foregroundTimer = new System.Windows.Forms.Timer(this.components);
+            this.controlsPanel = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.controlsPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // foregroundTimer
             // 
-            this.foregroundTimer.Interval = 50;
             this.foregroundTimer.Tick += new System.EventHandler(this.keepForeground);
+            // 
+            // controlsPanel
+            // 
+            this.controlsPanel.Controls.Add(this.button1);
+            this.controlsPanel.Controls.Add(this.richTextBox1);
+            this.controlsPanel.Controls.Add(this.pictureBox1);
+            this.controlsPanel.Location = new System.Drawing.Point(12, 12);
+            this.controlsPanel.Name = "controlsPanel";
+            this.controlsPanel.Size = new System.Drawing.Size(500, 425);
+            this.controlsPanel.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(160, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(180, 180);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 192);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(494, 96);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "Hi there writing test";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(150, 309);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // lockScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(519, 449);
+            this.Controls.Add(this.controlsPanel);
             this.Name = "lockScreen";
-            this.Text = "Form1";
+            this.Text = "Screen Blackout";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.onClosing);
+            this.controlsPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -52,6 +100,10 @@
         #endregion
 
         private System.Windows.Forms.Timer foregroundTimer;
+        private System.Windows.Forms.Panel controlsPanel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
